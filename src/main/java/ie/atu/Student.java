2decbdc6b4ae2084844669db57533656c80eefe6
joshua-battle <g00423199@atu.ie>
2023-11-01 13:13:33 +0000

    edited student default constructor

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -8,10 +8,8 @@ public class Student {
     String course;
 
     //constructor
-    public Student(String name, String email, String course) {
-        this.name = name;
-        this.email = email;
-        this.course = course;
+    public Student() {
+
     }
 
     public void getUserInput() {
