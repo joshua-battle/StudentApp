9e497498309caa6910d5020e267c4a4c99506997
joshua-battle <g00423199@atu.ie>
2023-11-01 12:44:53 +0000

    Adding constructor

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -4,4 +4,13 @@ public class Student {
     String name;
     String email;
     String course;
+
+    //constructor
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+
 }
