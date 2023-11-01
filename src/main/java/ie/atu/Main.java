2b2e6c1ac9395b5a9834388a4bfd4917019e2334
joshua-battle <g00423199@atu.ie>
2023-11-01 13:05:48 +0000

    Adding getuserinput

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,6 +3,13 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
+        //create new student object
+        Student s1 = new Student();
 
+        //get user input
+        s1.getUserInput();
+
+        //call display info method
+        s1.displayInfo();
     }
 }
--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,5 +1,7 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Student {
     String name;
     String email;
@@ -10,6 +12,19 @@ public class Student {
         this.name = name;
         this.email = email;
         this.course = course;
+    }
+
+    public void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter students name: ");
+        this.name = scanner.nextLine();
+
+        System.out.print("Enter students email: ");
+        this.email = scanner.nextLine();
+
+        System.out.print("Enter students course: ");
+        this.course = scanner.nextLine();
     }
 
     public void displayInfo() {
